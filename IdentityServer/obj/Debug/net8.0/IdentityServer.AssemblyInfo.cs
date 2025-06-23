fede5ecc389aeb522309fe9c031bc7acb80dcd28
ak-2002 <andrew.wanyonyi1@gmail.com>
2025-06-23 11:42:08 +0300

    fixing swagger ui authorization

--- IdentityServer/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
+++ IdentityServer/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a80d2db08a4a6916a3e689b88a46981a703f8df1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f5b9239003914033d48e24c1fe964a4453d3c571")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
