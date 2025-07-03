26d663948be0523654a73ca53a1652681e694e96
shemjay <jaokoshem@gmail.com>
2025-07-03 09:11:31 +0300

    rabbit mq error fixes

--- IdentityServer/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
+++ IdentityServer/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+72c6df82c647726526328148b98bf746108d597c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+20e65c49b378aa00aac53330295e1362f961a03d")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
