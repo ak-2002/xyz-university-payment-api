441898e2ff3fed2de2560abb0d788c747a7f8e6c
shemjay <jaokoshem@gmail.com>
2025-07-03 15:12:43 +0300

    fixed permissions for admin in studentcontroller v3

--- xyz-university-payment-api/obj/Debug/net8.0/xyz-university-payment-api.AssemblyInfo.cs
+++ xyz-university-payment-api/obj/Debug/net8.0/xyz-university-payment-api.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("xyz-university-payment-api")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+23466c36b551ebf73bec14b873484d0e96350c9c")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d2892c1db17cc684fde89a041c2b1a5b888d3a67")]
 [assembly: System.Reflection.AssemblyProductAttribute("xyz-university-payment-api")]
 [assembly: System.Reflection.AssemblyTitleAttribute("xyz-university-payment-api")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
