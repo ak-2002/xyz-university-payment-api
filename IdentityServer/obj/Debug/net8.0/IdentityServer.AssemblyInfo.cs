7fd705bacb529a70cdc2f40d3bac69f590d1451a
shemjay <jaokoshem@gmail.com>
2025-07-24 16:28:00 +0300

    deleted empty files and clean up repo

--- IdentityServer/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
+++ IdentityServer/obj/Debug/net8.0/IdentityServer.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+28271960a29acc031f3096a684bd654382d9549d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+918201602bc407eb01b9bd668a965c06d324adfe")]
 [assembly: System.Reflection.AssemblyProductAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IdentityServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
